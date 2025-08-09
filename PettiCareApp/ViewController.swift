398dabb4226953a99491f6ef7e3beed5b2b84bc7
aysenurkendirci <aysenurkendirciss@gmail.com>
2025-08-09 22:13:17 +0300

    Initial Commit

---
+++ PettiCareApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PettiCareApp
+//
+//  Created by Ayşe Nur Kendirci on 9.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
